bfe5148e7e13aa3abec99c2a8abf07e8d4ba8e30
kyouheicf <85217388+kyouheicf@users.noreply.github.com>
2024-04-02 16:09:57 +0900

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,5 +1,5 @@
 taskkill /IM cloudflared.exe /F
 if exist "%USERPROFILE%\.cloudflared" del /s/q "%USERPROFILE%\.cloudflared\*.*"
-start /B cloudflared.exe access rdp --hostname [access hostname] --url localhost:2244 --destination [workstation]:3389 > NUL
+start /B cloudflared.exe access rdp --hostname [access hostname] --url localhost:2244 > NUL
 mstsc /v:localhost:2244
 taskkill /IM cloudflared.exe /F
